cd26780cb3f340d2684b36dc6a16d463a13f90e5
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-30 23:36:17 +0530

    add template

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("c3ta");
+#!/usr/bin/env node
+
+import path from "path";
+import yargs from "yargs";
+import fs from "fs-extra";
+
+const args = yargs.option({
+  name: { type: "string", demandOption: true, alias: "n" },
+});
+
+const { name } = args;
+
+fs.mkdir(`./${name}`, (error: Error) => {
+  if (error) {
+    console.error("Could not create directory!");
+    process.exit(0);
+  }
+});
